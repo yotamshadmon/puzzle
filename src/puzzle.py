d73cb2cd8c693c2d6e102c9f296c2739ac264d92
yotamshadmon <34836259+yotamshadmon@users.noreply.github.com>
2018-04-25 14:54:40 +0300

    Initial commit

---
+++ src/puzzle.py
@@ -0,0 +1,111 @@
+import random
+	
+class pos:
+	def __init__(self, i, j):
+		self.i = i
+		self.j = j
+		
+class puzzle:
+	def __init__(self, n, m):
+		self.n = n
+		self.m = m	
+		self.board = [[j*n+i+1 for i in range(0, n)] for j in range(0, m)]
+		self.board[m-1][n-1] = 0
+		self.blank = pos(m-1, n-1)
+		
+	def validDirs(self, lastMove):
+		dirs = []
+		if self.blank.i > 0:
+			dirs.append('L')
+		if self.blank.i < self.n-1:
+			dirs.append('R')
+		if self.blank.j > 0:
+			dirs.append('U')
+		if self.blank.j < self.m-1:
+			dirs.append('D')
+		
+		if lastMove != None:
+			dirs.remove(self.opositeDir(lastMove))
+		
+#		print(dirs)
+		
+		return dirs
+		
+	def move(self, dir):
+#		print(dir)
+		validMove = 0
+		if dir == 'L':
+			if self.blank.i-1 >= 0: 
+				self.board[self.blank.j][self.blank.i], self.board[self.blank.j][self.blank.i-1] = self.board[self.blank.j][self.blank.i-1], self.board[self.blank.j][self.blank.i]
+				self.blank = pos(self.blank.i-1,self.blank.j);
+				validMove = 1
+		if dir == 'R':
+			if self.blank.i+1 < self.n: 
+				self.board[self.blank.j][self.blank.i], self.board[self.blank.j][self.blank.i+1] = self.board[self.blank.j][self.blank.i+1], self.board[self.blank.j][self.blank.i]
+				self.blank = pos(self.blank.i+1,self.blank.j);
+				validMove = 1
+		if dir == 'U':
+			if self.blank.j-1 >= 0: 
+				self.board[self.blank.j][self.blank.i], self.board[self.blank.j-1][self.blank.i] = self.board[self.blank.j-1][self.blank.i], self.board[self.blank.j][self.blank.i]
+				self.blank = pos(self.blank.i,self.blank.j-1);
+				validMove = 1
+		if dir == 'D':
+			if self.blank.j+1 < self.m: 
+				self.board[self.blank.j][self.blank.i], self.board[self.blank.j+1][self.blank.i] = self.board[self.blank.j+1][self.blank.i], self.board[self.blank.j][self.blank.i]
+				self.blank = pos(self.blank.i,self.blank.j+1);
+				validMove = 1
+#		self.printBoard()
+		return validMove
+
+	def opositeDir(self, dir):
+		if dir == 'U':
+			return 'D'
+		if dir == 'D':
+			return 'U'
+		if dir == 'R':
+			return 'L'
+		if dir == 'L':
+			return 'R'
+
+	def shuffle(self, level):
+		moveCount = 0
+		dir = None
+		while moveCount < level:
+			dirs = self.validDirs(dir)
+			dir = dirs[random.randint(0, len(dirs) - 1)]
+			moveCount += self.move(dir)
+#		print(moveCount)
+				
+	def printBoard(self):
+		for row in self.board:
+			for col in row:
+				print(col,'\t', end='')
+			print('\n', end='')
+		print('\n', end='')
+
+	def checkBoard(self):
+		ret = True
+		self.board[self.blank.j][self.blank.i] = self.m * self.n
+		for j in range(0, self.m):
+			for i in range(0, self.n):
+				if self.board[j][i] != j*self.n+i+1:
+					ret = False
+		self.board[self.blank.j][self.blank.i] = 0
+		return ret
+
+def main():
+	p = puzzle(4, 4)
+	p.shuffle(10)
+	p.printBoard()
+	
+	dir = None
+	while dir != 'Q':
+		dir = input('direction? ').upper()
+		p.move(dir)
+		p.printBoard()
+		if p.checkBoard() == True:
+			print('SUCCESS')
+			break
+		  
+if __name__== "__main__":
+  main()
